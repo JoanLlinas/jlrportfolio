b2f45ed61cc07710efdb79a1971dc7f20be11e5c
Joan Llinas <jlr02r@iesemilidarder.com>
2024-12-11 01:02:25 +0100

    Commit inicial

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path='/home' element={<HomePage/>}/>
         <Route path='/projects' element={<ProjectsPage/>}/>
         <Route path='/contact' element={<ContactPage/>}/>
